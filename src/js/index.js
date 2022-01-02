13c5d5eb232842f1ce7c994f546054eb0c2f04e6
Bethania Sanchez <bethasanchez3@gmail.com>
2022-01-02 17:56:23 -0400

    Fisrt changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,54 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+	return (
+		<div className="mainCounter">
+			<div className="clock">
+				{" "}
+				<i className="far fa-clock"></i>{" "}
+			</div>
+			<div className="fifth"> {props.fifthD % 10} </div>
+			<div className="fourth"> {props.fourthD % 10} </div>
+			<div className="third"> {props.thirdD % 10} </div>
+			<div className="second"> {props.secondD % 10} </div>
+			<div className="first"> {props.fisrtD % 10} </div>
+		</div>
+	);
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+SimpleCounter.PropTypes = {
+	fifthD: PropTypes.number,
+	fourthD: PropTypes.number,
+	thirdD: PropTypes.number,
+	secondD: PropTypes.number,
+	fisrtD: PropTypes.number,
+};
+
+let counter = 0;
+setInterval(function () {
+	const fifth = Math.floor(counter / 10000);
+	const fourth = Math.floor(counter / 1000);
+	const third = Math.floor(counter / 100);
+	const second = Math.floor(counter / 10);
+	const first = Math.floor(counter / 1);
+	counter++;
+
+	//render your react application
+	ReactDOM.render(
+		<SimpleCounter
+			fisrtD={first}
+			secondD={second}
+			thirdD={third}
+			fourthD={fourth}
+			fifthD={fifth}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
